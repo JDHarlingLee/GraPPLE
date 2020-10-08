01bb149eebe8eed0d10c6f0e71d37afcdbc3f358
JDHarlingLee <j.d.harling-lee@roslin.ed.ac.uk>
2020-10-08 13:57:02 +0100

    generate_edges now runs on single files - more useful and safer

--- scripts/generate_edges.sh
+++ scripts/generate_edges.sh
@@ -9,14 +9,11 @@
 
 while [ "$1" != "" ]; do
         case $1 in
-                -t | --thr_list )	    shift
-										thr_list=$1
+                -i | --input )	    	shift
+										input_file=$1
                                         ;;
-                -p | --paralogs )		shift
-										paralogs=1
-										;;
-				-d | --paralog_dir )	shift
-										paralog_dir=$1
+				-o | --output )			shift
+										output_folder=$1
 										;;
 				-q | --path )			shift
 										path=$1
@@ -24,18 +21,16 @@ while [ "$1" != "" ]; do
 				-n | --threads )		shift
 										threads=$1
 										;;
-				-h | --help )           echo "------------------------------------------------\n"
-										echo "Ensure you are executing this script within the output directory of your pangenome analysis\n"
-                                        echo "Runs from files PIRATE.all_alleles.wp.{thr}.tsv - at each threshold specified"
-										echo "-t | threshold list"
-										echo "-p | include paralogs or not. Default: off"
-										echo "-d | paralog_directory name - new files are created to avoid overwriting originals"
-										echo "-q | path to PIRATE directory - necessary for using PIRATE adapter scripts"
-										echo "-n | number of threads to use\n"
-										echo "------------------------------------------------"
+				-h | --help )           printf "\n------------------------------------------------\n\n"
+										printf "Ensure you are executing this script within the output directory of your pangenome analysis\n"
+										printf -- "-i | input file name - e.g. PIRATE.all_alleles.wp.90.tsv\n"
+										printf -- "-o | output folder. Default: pangenome_maps/\n"
+										printf -- "-q | path to PIRATE directory - necessary for using PIRATE adapter scripts\n"
+										printf -- "-n | number of threads to use. Default: 2\n"
+										printf "\n------------------------------------------------\n\n"
 										exit 0
                                         ;;
-                * )                     echo "Use -h | --help"
+                * )                     printf "Use -h | --help"
                                         exit 1
         esac
         shift
@@ -43,41 +38,31 @@ done
 
 
 # Check Inputs
-if test -z "$thr_list"; then
-        echo "ERROR: No thresholds supplied"
-        exit 1
+if test -z "$input_file"; then
+	printf "\nERROR: No input file provided"
+	exit 1
 fi
 
-if test -z "$paralogs"; then
-		paralogs=0
+if test -d "$output_folder"; then
+	printf "\nERROR: Output folder already exists"
+	exit 1
 fi
 
 if test -z "$path"; then
-        echo "ERROR: No path to PIRATE provided"
-        exit 1
+    printf "\nERROR: No path to PIRATE provided"
+    exit 1
 fi
 
 if test -z "$threads"; then
-        threads=2
+    threads=2
 fi
 
-for i in ${thr_list//,/ }
-do
-    if test -f PIRATE.all_alleles.wp.${i}.tsv; then
-        echo "PIRATE.all_alleles.wp.${i}.tsv not found"
-        exit 1
-    fi
-done
+# Set output file names
+output_file_tsv=${input_file%.tsv}.allelesAsGeneFamily.tsv
+output_file_edges=${input_file%.tsv}.allelesAsGeneFamily.edges
 
 # 1. Generate alleles as gene family files - necessary for edges/layout file on higher thresholds
-for i in ${thr_list//,/ }
-do
-	awk -F '\t' 'BEGIN { OFS = FS } ; { if (NR > 1) { $2 = $1 } } ; {print}' PIRATE.all_alleles.wp.${i}.tsv > pangenome_maps/PIRATE.all_alleles.wp.${i}.allelesAsGeneFamily.tsv 
-done
-
+awk -F '\t' 'BEGIN { OFS = FS } ; { if (NR > 1) { $2 = $1 } } ; {print}' $input_file > pangenome_maps/$output_file_tsv
 
 # 2. Generate edge files at each threshold specified
-for i in ${thr_list//,/ }
-do 
-   $path/scripts/pangenome_graph.pl --input with-paralogs/PIRATE.all_alleles.wp.${i}.allelesAsGeneFamily.tsv --output pangenome_maps/thr_${i}/ --gffs modified_gffs --no-cluster --gfa1 
-done
+$path/scripts/pangenome_graph.pl --input $output_file --output pangenome_maps/$output_file_edges --gffs ./modified_gffs/ --no-cluster --gfa1 
