f1762789b187e9f7599c8ea8caa3da3e07cbcebe
jdhl <j.d.harling-lee@roslin.ed.ac.uk>
2020-10-13 15:47:07 +0000

    minor typo and bug fixes - perl script file not opening still an issue

--- grapple/generate_edges.py
+++ grapple/generate_edges.py
@@ -1,14 +1,12 @@
 import argparse
 import csv
 import subprocess
-from os import path
+from pathlib import Path
+import os
 
-def generate_edges (input, output_dir, path, threads):
-    
-    if not path.exists(args.input):
-        print("ERROR: Input file not found")
+def generate_edges (input, output_dir, pirate_path, threads):
 
-    # Set 
+    # Set output file paths
     out_file=os.path.splitext(args.input)[0]
     out_file_tsv=out_file + ".allelesAsGeneFamily.tsv"
     out_file_edges=out_file + ".allelesAsGeneFamily.edges"
@@ -29,23 +27,24 @@ def generate_edges (input, output_dir, path, threads):
                     writer.writerow(new_row) 
 
     # 2. Generate edge file using PIRATE adapter script
-    script = "scripts\pangenome_graph.pl"
+    script = "scripts/pangenome_graph.pl"
     output = os.path.join(output_dir, out_file_edges)
-    pipe = subprocess.POpen(["perl",
-                            os.path.join(args.path, script),
+    pipe = subprocess.Popen(["perl",
+                            os.path.join(args.pirate_path, script),
                             "--input", out_file_tsv,
                             "--output", output,
-                            "--gffs", "./modified_gffs/",
-                            "--no-clutser",
-                            "--gfa1"])
+                            "--gffs", "/modified_gffs",
+                            "--no-cluster",
+                            "--gfa1"],
+			    stdin=subprocess.PIPE)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", type = str, required = True, help = "Input file - e.g. PIRATE.all_alleles.wp.90.tsv")
-    parser.add_argument("-o", "--output_dir", type = str, required = False, default = "pangenome_maps", help = "Output directory name")
-    parser.add_argument("-p", "--path", type = str, required = True, help = "Path to PIRATE folder")
+    parser.add_argument("-o", "--output_dir", type = Path, required = False, default = "pangenome_maps", help = "Output directory name")
+    parser.add_argument("-p", "--pirate_path", type = Path, required = True, help = "Path to PIRATE folder")
     parser.add_argument("-n", "--threads", type = int, required = False, default = 2, help = "Number of threads to use")
     args = parser.parse_args()
 
-    generate_edges(args.input, args.output, args.path, args.threads)
+    generate_edges(args.input, args.output_dir, args.pirate_path, args.threads)
